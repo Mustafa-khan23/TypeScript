660303010ffacadaf580827aa4677bb68a35649f
Mustafa-khan23 <mustafakhan3237@gmail.com>
2026-07-10 23:47:58 +0530

    Started with DataTypes in TS

---
+++ src/DataTypes/dataType.ts
@@ -0,0 +1,29 @@
+let numberValue: number = 1000;
+//number (float and integers)
+
+let bigintValue: bigint = 1000000n;
+//bigint (usually use for big number values)
+
+let stringValue: string = "I'm learning TypeScript";
+//string Type for sentence
+
+let array: number[] = [1, 2];
+//array declaration with number type
+
+let typedArray: [string, number] = ["M", 19];
+//array with specific type declaration
+
+let obj: object = {
+  key: "value",
+  key2: "value",
+};
+//Object declaration (key:value pairs)
+
+let declaredObj: { name: string; age: number } = {
+  name: "mustafa",
+  age: 19,
+};
+//Object declared with specific type of data
+
+let spread = { ...declaredObj };
+console.log(spread);
